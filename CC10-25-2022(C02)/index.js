d3525f8a0d0e2e4d2373e9dd230088ec01cdbee5
Saleh Abdullahi <salehabdullahi018@gmail.com>
2022-10-25 15:34:27 +0100

    2nd challenge of 25th october 2022

---
+++ CC10-25-2022(C02)/index.js
@@ -0,0 +1,18 @@
+//DESCRIPTION:
+//Write a function which converts the input string to uppercase.
+
+
+// SOLUTION
+
+// step 1:
+  //create a function that accept a string parameter
+  // convert the string to uppercase
+  // BONUS: what if the user enters a number? asnws:convert it to string
+
+
+//   function strUppercase(str){
+//     return  str.toString().toUpperCase()
+//   }
+
+let strUppercase = str=> str.toString().toUpperCase()
+  console.log(strUppercase('saleh'))
