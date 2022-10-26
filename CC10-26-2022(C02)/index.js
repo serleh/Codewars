9e22d5d4b7753d2da3680893bce37d4a780d93a5
Saleh Abdullahi <salehabdullahi018@gmail.com>
2022-10-26 21:40:57 +0100

    second challenge of 26 october 2022

---
+++ CC10-26-2022(C02)/index.js
@@ -0,0 +1,9 @@
+
+//code as fast as you can! You need to double the integer and return it.
+
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  i = i*2
+  return i;
+}
