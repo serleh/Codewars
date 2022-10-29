3181df1409b439064a0cbf5939b3f5a777f78f0a
Saleh Abdullahi <salehabdullahi018@gmail.com>
2022-10-29 15:43:28 +0100

    3rd challenge of 29th october 2022

---
+++ CC10-29-2022(C03)/index.js
@@ -0,0 +1,16 @@
+
+
+function opposite(n){
+    if(n === 0){
+        return 0
+    } else if (n < 0  ){
+        return n
+    }
+    else{
+        return -n
+    }
+
+    
+}
+
+console.log(opposite(11))
