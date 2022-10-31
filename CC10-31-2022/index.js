19bca04750ce92ed16dcc758eca31b8621d3ccd9
Saleh Abdullahi <salehabdullahi018@gmail.com>
2022-10-31 22:53:51 +0100

    31st october challenge

---
+++ CC10-31-2022/index.js
@@ -0,0 +1,18 @@
+//DESCRIPTION:
+//It's pretty straightforward. Your goal is to create a function
+// that removes the first and last characters
+// of a string. You're given one parameter, the original string. 
+//You don't have to worry with strings with less than two characters.
+
+
+                            // SOLUTION
+
+function firstXLastCha(str){
+   
+
+    return  str.slice(1,-1)
+
+    
+}
+
+console.log(firstXLastCha("Saleh"))
